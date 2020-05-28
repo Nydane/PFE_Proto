cc663ea1da79f247a1ddde576c98a36ade14e7ca
Maxime Moneyron <maxime.moneyron@gmail.com>
2020-05-28 20:54:49 +0200

    RaycastJump

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -10,11 +10,18 @@ public class BetterJump : MonoBehaviour
     [SerializeField]
     private float _playerJumpForce = 5f;
     [SerializeField]
-    public float fallMultiplier= 2.5f;
+    public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
     public float stopJumpTimer;
     public float timeInTheAir;
     Rigidbody rb;
+
+    [Header("RayCast")]
+    public GameObject[] downObj;
+    public float rayDownLength;
+
+
+    public GROUND_STATE groundState = GROUND_STATE.GROUNDED;
 
     private void Awake()
     {
@@ -25,56 +32,85 @@ public class BetterJump : MonoBehaviour
     private void Update()
     {
 
-        Debug.Log(Physics.gravity.y);
 
-        if (Input.GetKeyDown(KeyCode.JoystickButton0) && _CanJump == true)
-         {
-
+        if (!CheckGroundStatus())
+        {
             isJumping = true;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.JoystickButton0) && (!isJumping || CheckGroundStatus()))
+        {
+
+
             Player.playerInstance.moveVectorJump = Player.playerInstance.moveVector;
             Debug.Log("jump");
             rb.velocity = new Vector3(0f, _playerJumpForce, 0f);
-            _CanJump = false;
-
+            isJumping = true;
             
-         }
-
-        if (timeInTheAir >= stopJumpTimer)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
-        
-
-        if (rb.velocity.y <0)
+        else if (CheckGroundStatus() && timeInTheAir > Time.deltaTime)
         {
-             rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) *Time.deltaTime;
+            //_CanJump = true;
+            isJumping = false;
+        }
+
+
+
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
 
         }
         else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.JoystickButton0))
         {
-             rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
 
+        
         if (isJumping)
         {
-            isJumping = true;
             timeInTheAir += Time.deltaTime;
         }
         else if (!isJumping)
         {
             timeInTheAir = 0;
-            isJumping = false;
+
+        }
+
+
+        if (timeInTheAir >= stopJumpTimer)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
+
+
+
+    bool CheckGroundStatus()
+    {
+        
+        foreach (GameObject obj in downObj)
+        {
+            Ray downRay = new Ray(obj.transform.position, Vector3.down * rayDownLength);
+            Debug.DrawRay(obj.transform.position, Vector3.down * rayDownLength, Color.red);
+
+            if (Physics.Raycast(downRay, rayDownLength))
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+}
 
    
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            _CanJump = true;
-            isJumping = false;
-        }
-    }
 
+public enum GROUND_STATE
+{
+    GROUNDED,
+    AIRBORNE
 }
