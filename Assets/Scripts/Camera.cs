cd67ad3b29de9b1e8365627d58d7bf848b954c9c
Maxime Moneyron <maxime.moneyron@gmail.com>
2020-06-03 18:29:09 +0200

    Camera Verticale

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -13,8 +13,12 @@ public class Camera : MonoBehaviour
     public float horizontalOffset = 2.0f;
     public float constantHeight = 15f;
     public float lerpTime = 0.1f;
-   
     public Vector3 rotationCamera;
+
+    private void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void LateUpdate()
@@ -30,6 +34,41 @@ public class Camera : MonoBehaviour
         transform.rotation = Quaternion.Euler(rotationCamera);
 
 
-        
+         /*if (targetFocus.transform.position.y >= 7.5)
+         {
+             constantHeight = 20f;
+         }
+         else constantHeight = 15f;*/
+
+        if (!BetterJump.isJumping)
+        {
+            constantHeight = GetCameraheight(15, 1.9f, 5.5f, 5f, targetFocus.transform.position.y);
+
+        }
+
+
     }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="baseCamHeight">constant Height : hauteur minimum de la camera</param>
+    /// <param name="baseHeight">hauteur minimum du player</param>
+    /// <param name="deltaX">monter en Y que doit prendre le joueur avant que la camera bouge</param>
+    /// <param name="deltaY">réaction de la caméra a chaque fois qu'une step est passé</param>
+    /// <param name="y">hauteur actuelle du joueur</param>
+    /// <returns></returns>
+    float GetCameraheight(float baseCamHeight, float baseHeight, float deltaX, float deltaY, float y)
+    {
+        int i = 0;
+        float posY = y;
+        while(posY - deltaX >= baseHeight)
+        {
+            i++;
+            posY -= deltaX;
+                
+        }
+
+        return baseCamHeight + i * deltaY;
+    }
+    
 }
